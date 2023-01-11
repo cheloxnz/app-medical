cb67a78eb7733d18b0e8b622aca24b8866b6d4b9
marcelo <cheloxnz@gmail.com>
2023-01-11 18:00:13 -0300

    subiendo app medical

---
+++ src/components/SideBar.vue
@@ -0,0 +1,98 @@
+<template>
+    <nav>
+        <v-navigation-drawer app dark color="#dcebec">
+            <div class="text-center mt-5">
+                <h1 class="text-lg-h5 font-weight-bold">CHL MEDICAL</h1>
+                <v-divider color="white" class="mt-4"></v-divider>
+                <v-avatar class="my-5" size="100">
+                    <v-img src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460" class="image"></v-img>
+                </v-avatar>
+
+                <h1 class="black--text mb-2">Doctor Charles</h1>
+                <h6 class="grey--text mb-2">admin@admin.com</h6>
+                <v-btn rounded color="#093b5b" outlined>Book Appointment</v-btn>
+            </div>
+            <v-divider class="mx-10 mt-3"></v-divider>
+            <v-list dense class="mt-5">
+                <v-list-item-group v-model="selectedItem" color="white">
+                    <v-list-item v-for="(item, i) in items" :key="i" v-slot="{ active }" @click="listPersons()">
+                        <v-list-item-icon class="ml-6">
+                            <v-icon v-text="item.icon" :color="active ? 'black' : 'grey'"></v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title v-text="item.text"
+                                :class="active ? 'black--text' : 'grey--text'"></v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item class="mt-16" v-slot="{ active }">
+                        <v-list-item-icon class="ml-6">
+                            <v-icon :color="active ? 'black' : 'grey'">mdi-exit-to-app</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content @click="$router.push('login')">
+                            <v-list-item-title
+                                :class="active ? 'black--text' : 'grey--text'">Logout</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar app color="#F5F6FA" flat>
+            <v-text-field dense label="Search..." color="black" class="mt-4"></v-text-field>
+            <v-spacer></v-spacer>
+            <v-btn class="ml-2" icon small>
+                <v-icon dark>
+                    mdi-bell
+                </v-icon>
+            </v-btn>
+            <v-btn class="ml-2" icon small>
+                <v-icon dark>
+                    mdi-email
+                </v-icon>
+            </v-btn>
+            <v-btn class="ml-3" fab small color="#6B6B6E">
+                <v-icon dark large color="white">
+                    mdi-account
+                </v-icon>
+            </v-btn>
+        </v-app-bar>
+        <Home />
+    </nav>
+</template>
+
+<script>
+import Home from "../views/Home.vue";
+
+export default {
+    components: {
+        Home,
+    },
+    data: () => ({
+        doctor: null,
+        selectedItem: 0,
+        items: [
+            { icon: 'mdi-finance', text: 'Dashboard' },
+            { icon: 'mdi-calendar-outline', text: 'Appointment' },
+            { icon: 'mdi-account-outline', text: 'Doctors' },
+            { icon: 'mdi-content-paste', text: 'Patients' },
+            { icon: 'mdi-map-marker-outline', text: 'Location' },
+            { icon: 'mdi-cogs', text: 'Settings' },
+        ]
+    }),
+    methods: {
+        listPersons() {
+            this.items.filter((e) => {
+               return this.doctor = e.text == "Doctors";
+            });
+            if(this.doctor) {
+                this.$router.push('/doctors')
+            }
+        }
+    },
+}
+</script>
+
+<style>
+.image {
+    border: 2px solid #2EBFAF
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app
+    id="inspire"
+    :style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+    <v-divider color="white" class="mt-1" dark></v-divider>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-toolbar flat color="rgba(0,0,0,0)" class="mt-n5">
+            <v-toolbar-title>Dashboard</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+
+          <v-row class="px-5 mt-n6 ml-5">
+            <v-col cols="12" sm="3" v-for="list in lists" :key="list">
+              <v-card
+                align="center"
+                color="#F9FAFC"
+                class="rounded-circle border pt-10"
+                width="200"
+                height="200"
+                flat
+              >
+                <v-icon size="60" color="#093b5b">
+                  {{ list.icon }}
+                </v-icon>
+
+                <v-card-text class="grey--text text-lg-h6">
+                  {{ list.title }}
+                </v-card-text>
+
+                <v-btn
+                  absolute
+                  color="#093b5b"
+                  class="white--text"
+                  fab
+                  left
+                  top
+                >
+                  <h2>{{ list.count }}</h2>
+                </v-btn>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row class="mt-n5">
+            <v-col cols="12" sm="7">
+            </v-col>
+          </v-row>
+          <v-row class="mt-n12">
+            <v-col cols="12" sm="12">
+             <Table />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Table from "../components/Table.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Table,
   },
+  data: () => ({
+   
+    lists: [
+      {
+        icon: "fas fa-user-md",
+        title: "Doctors",
+        count: 21,
+      },
+      {
+        icon: "fas fa-procedures",
+        title: "Patients",
+        count: 41,
+      },
+      {
+        icon: "fas fa-user-nurse",
+        title: "Nurses",
+        count: 35,
+      },
+      {
+        icon: "fas fa-hospital",
+        title: "Directors",
+        count: 4,
+      },
+    ],
+  }),
 };
 </script>
+<style scoped>
+.border {
+  border: 3px solid #dcebec !important;
+}
+.v-btn--fab.v-size--default.v-btn--absolute.v-btn--top {
+  top: 65px !important;
+}
+.v-btn--absolute.v-btn--left,
+.v-btn--fixed.v-btn--left {
+  left: -26px !important;
+}
+</style>
